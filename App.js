94e3f5ad4155105c4d3552a7f98f55d9d1c8c28b
Hetal Kanani <hetalkanani12002@gmail.com>
2023-08-08 12:31:40 +0530

    changed project name

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ const SplashScreen = () => {
         <Image
               style={splash.image}
               source={require('./assets/images/Brainiac.png')}/>
-         <Text style={splash.txt}> Brainiac </Text>     
+         <Text style={splash.txt}> Test</Text>     
       </View>
   );
 }
